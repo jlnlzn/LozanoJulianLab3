ec8c0b1a26d4ea7b26514c14d98193f5f90e6c4b
jlnlzn <42483441+jlnlzn@users.noreply.github.com>
2018-09-22 20:59:19 +0800

    Initial commit

---
+++ app/src/main/java/julian/lozano/com/lozanojulianlab3/activity_lozano_julian_lab3.java
@@ -0,0 +1,13 @@
+package julian.lozano.com.lozanojulianlab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_lozano_julian_lab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lozano_julian_lab3);
+    }
+}
